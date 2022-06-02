7aa5410d1515ac0056280ebf73384d8485137631
erikfarhanmalik <erik.farhan.malik@mhealth.tech>
2022-06-02 17:09:56 +0700

    Added support to add all pages instead of only put the first page.

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module merge-pdf
 go 1.17
 
 require (
+	github.com/dslipak/pdf v0.0.1
 	github.com/phpdave11/gofpdf v1.4.2
+)
+
+require (
 	github.com/phpdave11/gofpdi v1.0.12 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,28 +1,37 @@
 package main
 
 import (
+	"log"
+
+	"github.com/dslipak/pdf"
 	"github.com/phpdave11/gofpdf"
 	"github.com/phpdave11/gofpdf/contrib/gofpdi"
 )
 
 func main() {
 	var err error
-	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdfResult := gofpdf.New("P", "mm", "A4", "")
 	filePaths := []string{
 		"example-pdf-1.pdf",
 		"example-pdf-2.pdf",
 	}
 	for _, path := range filePaths {
-		addPdf(pdf, path)
+		f, err := pdf.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		addPdf(pdfResult, path, f.NumPage())
 	}
-	err = pdf.OutputFileAndClose("example.pdf")
+	err = pdfResult.OutputFileAndClose("example.pdf")
 	if err != nil {
 		panic(err)
 	}
 }
 
-func addPdf(pdf *gofpdf.Fpdf, path string) {
-	template := gofpdi.ImportPage(pdf, path, 1, "/MediaBox")
-	pdf.AddPage()
-	gofpdi.UseImportedTemplate(pdf, template, 0, 0, 210, 0)
+func addPdf(pdf *gofpdf.Fpdf, path string, pageNumber int) {
+	for i := 1; i <= pageNumber; i++ {
+		template := gofpdi.ImportPage(pdf, path, i, "/MediaBox")
+		pdf.AddPage()
+		gofpdi.UseImportedTemplate(pdf, template, 0, 0, 210, 0)
+	}
 }
